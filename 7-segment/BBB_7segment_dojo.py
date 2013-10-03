f9ddcdf64cbab155a434925a9d2b76b172cdb0c6
Rafael M. Lopes <rafael@rafaelme.com>
2013-10-02 21:32:14 -0300

    portas corrigidas e ADC.read

--- 7-segment/BBB_7segment_dojo.py
+++ 7-segment/BBB_7segment_dojo.py
@@ -5,14 +5,14 @@ import Adafruit_BBIO.GPIO as GPIO
 import Adafruit_BBIO.ADC as ADC
 import time
 
-a = "P8_12"
-b = "P8_14"
-c = "P8_16"
-d = "P8_18"
-e = "P8_20"
-f = "P8_22"
-g = "P8_24"
-p = "P8_26"
+a = "P8_12" # topo
+b = "P8_13" # direita cima
+c = "P8_14" # direita baixo
+d = "P8_15" # baixo
+e = "P8_16" # esquerda baixo
+f = "P8_17" # esquerda cima
+g = "P8_18" # meio
+p = "P8_19" # ponto
 pot = "P9_33"
 
 GPIO.setup(pot, GPIO.IN)
@@ -30,6 +30,6 @@ ADC.setup()
 while True:
     for i in [f, a, b, c, d, e]:
        GPIO.output(i, GPIO.HIGH)
-       delay = ADC.read_raw(pot)
+       delay = ADC.read(pot)
        time.sleep(delay)
        GPIO.output(i, GPIO.LOW)
